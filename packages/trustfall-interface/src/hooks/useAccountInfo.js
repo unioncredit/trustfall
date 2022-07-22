4fdcd53496eeda9aa6a3906748e350b6b03c5f35
Gerald <gm@gerald.host>
2022-07-22 23:58:19 +0100

    feat: arbitrum support

--- packages/trustfall-interface/src/hooks/useAccountInfo.js
+++ packages/trustfall-interface/src/hooks/useAccountInfo.js
@@ -1,6 +1,6 @@
 import { utils } from "ethers";
 import { Contract } from "@ethersproject/contracts";
-import { Kovan, useCalls, useEthers } from "@usedapp/core";
+import { Arbitrum, Kovan, useCalls, useEthers } from "@usedapp/core";
 
 import UserManagerABI from "abis/userManager.json";
 
@@ -8,6 +8,7 @@ const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
 
 export const userManagerAddress = {
   [Kovan.chainId]: "0x391fDb669462FBAA5a7e228f3857281BeCf235EE",
+  [Arbitrum.chainId]: "0xb71F3D4342AaE0b8D531E14D2CF2F45d6e458A5F",
 };
 
 const userManagerIft = new utils.Interface(UserManagerABI);
@@ -16,7 +17,9 @@ export const userManager = new Contract(ZERO_ADDRESS, userManagerIft);
 export default function useAccountInfo() {
   const { account, chainId } = useEthers();
 
-  const attachedUserManager = userManager.attach(userManagerAddress[chainId]);
+  const attachedUserManager =
+    userManagerAddress[chainId] &&
+    userManager.attach(userManagerAddress[chainId]);
 
   const calls = [
     {
@@ -46,14 +49,15 @@ export default function useAccountInfo() {
     },
   ];
 
-  const results = useCalls(calls);
+  const results = useCalls(userManagerAddress[chainId] && calls);
 
   return (
+    userManagerAddress[chainId] &&
     results &&
     results.reduce(
       (acc, result, i) => ({
         ...acc,
-        [calls[i].method]: result?.value[0],
+        [calls[i].method]: result?.value?.[0],
       }),
       {}
     )
--- packages/trustfall-interface/src/index.js
+++ packages/trustfall-interface/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { DAppProvider, Kovan } from "@usedapp/core";
+import { Arbitrum, DAppProvider, Kovan } from "@usedapp/core";
 import { setGridConfiguration } from "@unioncredit/ui";
 import { BrowserRouter as Router } from "react-router-dom";
 
@@ -9,15 +9,16 @@ import App from "./App";
 import "./index.scss";
 
 const config = {
-  readOnlyChainId: Kovan.chainId,
+  readOnlyChainId: Arbitrum.chainId,
   readOnlyUrls: {
-    [Kovan.chainId]:
-      "https://kovan.infura.io/v3/b3359a5636d64b858b26fc5cccab8578",
+    [Arbitrum.chainId]:
+      "https://arbitrum-mainnet.infura.io/v3/b3359a5636d64b858b26fc5cccab8578",
   },
   multicallAddresses: {
     [Kovan.chainId]: "0x2cc8688c5f75e365aaeeb4ea8d6a480405a48d2a",
+    [Arbitrum.chainId]: "0x7a7443f8c577d537f1d8cd4a629d40a3148dd7ee",
   },
-  networks: [Kovan],
+  networks: [Arbitrum, Kovan],
 };
 
 setGridConfiguration({
