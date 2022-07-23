ef7d303d7a49655fe4f63ad8c20a4abac7e4edbc
Gerald <gm@gerald.host>
2022-07-23 12:06:04 +0100

    fix: scoring ordering

--- packages/trustfall-interface/src/hooks/useTokenHolders.js
+++ packages/trustfall-interface/src/hooks/useTokenHolders.js
@@ -15,45 +15,59 @@ export default function useTokenHolders() {
       const nft = contract.attach(nftAddress[chainId]).connect(library);
       const id = await nft.id();
 
+      const um = userManager
+        .attach(userManagerAddress[chainId])
+        .connect(library);
+
       const resp = await Promise.all(
         Array(Number(id.toString()))
           .fill(0)
           .map(async (_, i) => {
             const address = await nft.ownerOf(i + 1);
 
-            const um = userManager
-              .attach(userManagerAddress[chainId])
-              .connect(library);
-
             const isMember = await um.checkIsMember(address);
             const stakers = await um.getStakerAddresses(address);
-            const vouches = await Promise.all(
-              stakers.map((staker) => um.getVouchingAmount(staker, address))
-            );
-
-            const vouchesSum = vouches.reduce(
-              (acc, vouch) => acc.add(vouch),
-              BigNumber.from(0)
-            );
 
             return {
               address,
               stakers,
-              vouches: vouches.map((v, i) => [stakers[i], v]),
-              vouchesSum,
               isMember,
             };
           })
       );
 
-      const pairTotals = getPairTotals(resp);
+      const holderAddresses = resp.map((r) => r.address.toLowerCase());
 
-      setData(
-        resp.map((row) => ({
-          ...row,
-          score: getScore(row.vouches, pairTotals),
-        }))
+      const respWithVouches = await Promise.all(
+        resp.map(async (row) => {
+          const vouches = await Promise.all(
+            row.stakers
+              .filter((staker) =>
+                holderAddresses.includes(staker.toLowerCase())
+              )
+              .map(async (staker) => [
+                staker,
+                await um.getVouchingAmount(staker, row.address),
+              ])
+          );
+
+          const vouchesSum = vouches.reduce(
+            (acc, vouch) => acc.add(vouch[1]),
+            BigNumber.from(0)
+          );
+
+          return { ...row, vouches, vouchesSum };
+        })
       );
+
+      const pairTotals = getPairTotals(respWithVouches);
+
+      const respWithScores = respWithVouches.map((row) => ({
+        ...row,
+        score: getScore(row.vouches, pairTotals),
+      }));
+
+      setData(respWithScores.sort((a, b) => b.score - a.score));
     }
 
     fetchData();
