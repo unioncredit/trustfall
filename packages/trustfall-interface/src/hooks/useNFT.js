423473e60be15ee26467c92d232855e354f621b4
Gerald <gm@gerald.host>
2022-07-28 17:00:51 +0100

    feat: TRUSTV2 arbitrum

--- packages/trustfall-interface/src/hooks/useNFT.js
+++ packages/trustfall-interface/src/hooks/useNFT.js
@@ -13,7 +13,7 @@ import ABI from "abis/nft.json";
 export const itf = new utils.Interface(ABI);
 export const nftAddress = {
   [Kovan.chainId]: "0xf4Fa9801E89754C8A587253A835f15C035290a1c",
-  [Arbitrum.chainId]: "0x8ce86b36add6b60229ba2f1bdeac220244c8ad24",
+  [Arbitrum.chainId]: "0xafb9E64d8B84C20E21299E0468d57FCC04F14Acd",
 };
 
 const ZERO_ADDRESS = "0x0000000000000000000000000000000000000000";
