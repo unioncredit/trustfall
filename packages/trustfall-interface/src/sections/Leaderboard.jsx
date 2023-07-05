ee9238682c4accc6d93b95ffe214c1bed8670203
Gerald <gm@gerald.host>
2023-07-05 11:31:44 +0100

    fix: mint balance check

--- packages/trustfall-interface/src/sections/Leaderboard.jsx
+++ packages/trustfall-interface/src/sections/Leaderboard.jsx
@@ -14,6 +14,7 @@ import Share from "components/Share";
 import { Link } from "react-router-dom";
 import TeamStats from "components/TeamStats";
 import useTeamAddresses from "../hooks/useTeamAddresses";
+import { BigNumber } from "ethers";
 
 function Leaderboard() {
   const [view, setView] = useState("pvp"); // pvp, teams
@@ -21,11 +22,32 @@ function Leaderboard() {
 
   const { account } = useEthers();
   const { data: holders, refresh } = useTokenHolders();
-  const { value: balances } = useTeamCall(
+  const { value: cyanBalance } = useTeamCall(
     "cyan",
     "balanceOf",
     account ? [account] : null
   );
+  const { value: magentaBalance } = useTeamCall(
+    "magenta",
+    "balanceOf",
+    account ? [account] : null
+  );
+  const { value: yellowBalance } = useTeamCall(
+    "yellow",
+    "balanceOf",
+    account ? [account] : null
+  );
+  const { value: blackBalance } = useTeamCall(
+    "black",
+    "balanceOf",
+    account ? [account] : null
+  );
+
+  const balances =
+    cyanBalance?.[0]
+      ?.add(magentaBalance?.[0] || BigNumber.from(0))
+      ?.add(yellowBalance?.[0] || BigNumber.from(0))
+      ?.add(blackBalance?.[0] || BigNumber.from(0)) || BigNumber.from(0);
 
   const test = useTeamAddresses("cyan");
   console.log(test);
@@ -34,17 +56,32 @@ function Leaderboard() {
     <>
       <Grid.Row>
         <Grid.Col>
-          <Text size="small" className="Leaderboard__intro">Embrace the thrill of trust-based finance in Trustfall, a game of alliances, risks, and rewards. Trustfall interweaves collective strategy and individual audacity, as players build credit lines through earning and granting vouches. With color-coded Team NFTs, you're more than an individual—you're part of a community, competing and collaborating in a dynamic financial ecosystem. It’s not just a game, it's an exploration of trust, deceit, and collective power. Are you ready to take the fall?</Text>
+          <Text size="small" className="Leaderboard__intro">
+            Embrace the thrill of trust-based finance in Trustfall, a game of
+            alliances, risks, and rewards. Trustfall interweaves collective
+            strategy and individual audacity, as players build credit lines
+            through earning and granting vouches. With color-coded Team NFTs,
+            you're more than an individual—you're part of a community, competing
+            and collaborating in a dynamic financial ecosystem. It’s not just a
+            game, it's an exploration of trust, deceit, and collective power.
+            Are you ready to take the fall?
+          </Text>
 
           <Box mt="16px">
-            <Link to="/rules#howtojoin" className="Leaderboard__link">How to join</Link>
-            <Link to="/rules#gamerules" className="Leaderboard__link">Game rules</Link>
-            <Link to="/rules#prizes" className="Leaderboard__link">Prizes</Link>
+            <Link to="/rules#howtojoin" className="Leaderboard__link">
+              How to join
+            </Link>
+            <Link to="/rules#gamerules" className="Leaderboard__link">
+              Game rules
+            </Link>
+            <Link to="/rules#prizes" className="Leaderboard__link">
+              Prizes
+            </Link>
           </Box>
         </Grid.Col>
         <Grid.Col>
-          {account && (
-            (balances?.[0].gt(0) ? (
+          {account &&
+            (balances.gt(0) ? (
               <MyStats data={holders} />
             ) : (
               <ClaimNFT
@@ -52,35 +89,32 @@ function Leaderboard() {
                 accountBalance={balances?.[0]}
                 setShowShare={setShowShare}
               />
-            ))
-          )}
+            ))}
         </Grid.Col>
       </Grid.Row>
       <Grid.Row>
         <Grid.Col>
           <Box fluid mt="48px" direction="vertical">
             <Box mb="16px" justify="space-between" align="center" fluid>
-              <Heading m={0} className="Leaderboard__heading">Leaderboard</Heading>
+              <Heading m={0} className="Leaderboard__heading">
+                Leaderboard
+              </Heading>
 
               <Box className="Leaderboard__controls">
                 <Button
                   label="PVP"
                   onClick={() => setView("pvp")}
-                  className={cn({"active": view === "pvp"})}
+                  className={cn({ active: view === "pvp" })}
                 />
                 <Button
                   label="Teams"
                   onClick={() => setView("teams")}
-                  className={cn({"active": view === "teams"})}
+                  className={cn({ active: view === "teams" })}
                 />
               </Box>
             </Box>
 
-            {view === "pvp" ? (
-              <Table data={holders} />
-            ) : (
-              <TeamStats />
-            )}
+            {view === "pvp" ? <Table data={holders} /> : <TeamStats />}
           </Box>
         </Grid.Col>
       </Grid.Row>
